9cd53aaf345838f792dc9a8561bd3401a0f6b1ae
joao castro <joaocastro_125@hotmail.com>
2022-09-05 17:14:37 -0300

    projeto calculadora

---
+++ script.js
@@ -0,0 +1,25 @@
+// @ts-nocheck
+let primeiroSemestre=Number(prompt("informe nota do primeiroSemestre:"))
+let segundoSemestre=Number(prompt("informe nota do segundoSemestre:"))
+let terceiroSemestre=Number(prompt("informe nota do terceiroSemestre:"))
+let quartoSemestre=Number(prompt("informe nota do quartoSemestre:"))
+
+let resultadoFinal=Number((primeiroSemestre+segundoSemestre+terceiroSemestre+quartoSemestre))/4
+let notaFracionada=Number(resultadoFinal.toFixed(1))
+
+if(resultadoFinal>=10){
+    document.querySelector('.resultado').innerHTML="aprovado"
+    document.querySelector('.resultado').innerHTML="aprovado:"+resultadoFinal
+}
+else if(resultadoFinal>=5){
+    document.querySelector('.resultado').innerHTML="esta de recuperação:"+resultadoFinal
+}
+else{
+    
+    document.querySelector('.resultado').innerHTML="reprovado:"+resultadoFinal
+   
+}
+
+
+
+
